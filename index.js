77c387826b667533338a27f8d00f5a9da88a9b39
Bhuvan <bhuvanmathranda85@gmail.com>
2026-02-10 21:12:39 +0530

    feature ci-pipeline

---
+++ index.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    console.log("Addition result:")
+    console.log(a+b);
+}
+
+console.log("Addition of two values:")
+let a = 10;
+let b = 20;
+
+add(a,b);
+console.log("Function is completed!")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Tests are running successfully!")
+setTimeout(()=> console.log("Waiting to run test... 3 seconds wait"),3000);
+console.log("Test ran successfully!")
